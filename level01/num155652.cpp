1df485f99d78b64ac74687a54ab8791b10ea460b
Lsapee <dlworhks94@gmail.com>
2023-02-02 23:28:18 +0900

    프로그래머스 1레벨 둘만의 암호

---
+++ level01/num155652.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char c[51];
+
+int main(){
+    string s ="aukks";
+    string skip = "wbqd";
+    int index = 5;
+    sort(skip.begin(),skip.end());
+    for(int i=0; i<s.size(); i++){
+        c[i]= s[i];
+        for(int j=0; j<index; j++){
+            if(c[i]=='z') {
+                c[i] = 'a';
+            }else{
+                c[i] +=1;
+            }
+            for(int k=0; k<skip.size(); k++){
+                if(c[i]==skip[k]){
+                    k=-1;// skip을 정렬해서 a~z순서대로 만들었지만 z에서 a로 갔을때 스킵에 a가 있으면 에러 발생하기에 넣어줌
+                    if(c[i]=='z') {
+                        c[i] = 'a';
+                    }else{
+                        c[i]+=1;
+                    }
+                }
+            }
+        }
+    }
+    string answer ="";
+    for(int i=0; i<s.size(); i++){
+        answer += c[i];
+    }
+    cout<<answer;
+    return 0;
+}
+//vwxyz
