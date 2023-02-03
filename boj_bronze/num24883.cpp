fbb68fd7a76bfb580fc815f1facfa10b7ed09e76
Lsapee <dlworhks94@gmail.com>
2023-02-03 20:54:15 +0900

    백준 자동완성

---
+++ boj_bronze/num24883.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    char c;
+    cin>>c;
+
+    if(c=='n'||c=='N'){
+        cout<<"Naver D2";
+    }else{
+        cout<<"Naver Whale";
+    }
+}
