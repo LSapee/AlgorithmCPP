1f4c88a1b787642a32f31ac91bfbf6e0b9919555
Lsapee <dlworhks94@gmail.com>
2023-02-05 15:09:50 +0900

    백준 몇개고?

---
+++ boj_bronze/num27294.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n,m;
+    cin>>n>>m;
+
+    if(n>=12 && n<=16 &&m==0){
+        cout<<320;
+    }else{
+        cout<<280;
+    }
+    return 0;
+}
