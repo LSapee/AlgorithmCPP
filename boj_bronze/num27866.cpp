88fd3a21bdc6168ad8d792df8f26995bb9e219e4
Lsapee <dlworhks94@gmail.com>
2023-03-09 21:57:10 +0900

    백준 문자와 문자열

---
+++ boj_bronze/num27866.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    int n;
+    cin>>s;
+    cin>>n;
+
+    cout<<s[n-1];
+}
