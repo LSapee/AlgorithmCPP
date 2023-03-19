72fb687739734546016840be8ab7d0652d644ff4
Lsapee <dlworhks94@gmail.com>
2023-03-19 22:06:37 +0900

    백준 Metronome

---
+++ boj_bronze/num27389.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    double n;
+    cin>>n;
+    cout<<fixed;
+    cout.precision(2);
+    cout<<n/4;
+}
