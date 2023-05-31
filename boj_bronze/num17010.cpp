01910335b931b3c939b0e6b900b00fb9e81e932c
Lsapee <dlworhks94@gmail.com>
2023-05-31 21:17:32 +0900

    boj Time to Decompress

---
+++ boj_bronze/num17010.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n; cin>>n;
+    for(int i=0; i<n; i++){
+        int a;cin>>a;
+        char b; cin>>b;
+        for(int j =0; j<a; j++){
+            cout<<b;
+        }
+        cout<<"\n";
+    }
+}
