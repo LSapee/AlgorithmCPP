122dc60c800f7bc838e258e49995737346c2353f
Lsapee <dlworhks94@gmail.com>
2023-06-05 12:45:48 +0900

    leetcode Can Make Arithmetic Progression From Sequence

---
+++ leetCode/num1502.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int k = arr[1]-arr[0];
+        bool ans = true;
+        for(int i=0; i<arr.size()-1; i++){
+            if(arr[i+1]-arr[i]!=k) {
+                ans=false;
+                break;
+            }
+        }
+        return ans;
+    }
+
+};
