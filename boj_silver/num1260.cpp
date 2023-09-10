3bd2ca53f2c7ebaf89e526318da4594cf95ff964
LSapee <dlworhks94@gmail.com>
2023-09-10 23:18:11 +0900

    boj DFS와 BFS

--- boj_silver/num1260.cpp
+++ boj_silver/num1260.cpp
@@ -1,55 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> A;
+
 bool visited[1004];
 bool visited2[1004];
 int n,m,st;
+
+void bfs(vector<vector<int>> A){
+    queue<int> Q;
+    Q.push(st);
+    visited[st] = true;
+    while(!Q.empty()){
+        int k =Q.front();Q.pop();
+        cout<< k <<" ";
+        for(auto a : A[k]){
+            if(visited[a])continue;
+            Q.push(a);
+            visited[a]= true;
+        }
+    }
+    cout<<"\n";
+}
+void dfs(vector<vector<int>> A){
+    stack<int> stk;
+    stk.push(st);
+    while(!stk.empty()){
+        int k =stk.top(); stk.pop();
+        if(visited2[k])continue;
+        visited2[k] = true;
+        cout<< k<<" ";
+        for(int i=A[k].size()-1; i>=0; i--){
+            int a =A[k][i];
+            if(visited2[a])continue;
+            stk.push(a);
+        }
+    }
+    cout<<"\n";
+}
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin>>n>>m>>st;
-    A.resize(n);
-    for(int i=0; i<m; i++){
+    vector<vector<int>> A;
+    A.resize(n+3,vector<int>(0));
+    while(m--){
         int x,y;cin>>x>>y;
         A[x].push_back(y);
         A[y].push_back(x);
     }
-    for(int i=0; i<n+1; i++){
-        sort(A[i].begin(),A[i].end());
-    }
-    stack<int> stk;
-    stk.push(st);
-    visited2[st] = true;
-    while(!stk.empty()){
-        int k = stk.top(); stk.pop();
-        cout<<k<<" ";
-        for(int i=0; i<A[k].size(); i++){
-            int num = A[k][i];
-            if(!visited2[num]){
-                stk.push(num);
-                visited2[num]=true;
-                break;
-            }
-        }
-
-    }
-    cout<<"\n";
-    queue<int> Q;
-    Q.push(st);
-    visited[st] = true;
-    while(!Q.empty()){
-        int c = Q.front(); Q.pop();
-        cout<<c<<" ";
-        for(int i=0; i<A[c].size(); i++){
-            int num = A[c][i];
-            if(!visited[num]){
-                Q.push(num);
-                visited[num] =true;
-            }
-        }
-    }
+    for(int i=0; i<A.size(); i++)sort(A[i].begin(),A[i].end());
+    dfs(A);
+    bfs(A);
 
     return 0;
 }
