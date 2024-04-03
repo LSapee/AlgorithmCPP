b33c6a30242a0b0e8f8c3627947a5324bce41c34
LSapee <dlworhks94@gmail.com>
2024-04-03 20:28:21 +0900

    boj 창문 닫기

---
+++ boj_silver/problem/num13909.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;cin>>n;
+    int cnt = sqrt(n);
+    cout<<cnt;
+}
