36be906ee81e929cb5935fde755ac6e112986f13
LSapee <dlworhks94@gmail.com>
2024-07-11 21:34:42 +0900

    leetcode Duplicate Zeros

---
+++ leetCode/num1089.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]==0){
+                for(int j=arr.size()-1; j>i+1; j--) swap(arr[j],arr[j-1]);
+                if(i+1<arr.size())arr[i+1] = 0;
+                i++;
+            }
+        }
+    }
+};
