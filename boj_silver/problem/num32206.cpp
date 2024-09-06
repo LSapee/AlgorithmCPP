c5f58f5a5b28ac85d612bb7383aa8e2b7de53b33
LSapee <dlworhks94@gmail.com>
2024-09-06 17:26:10 +0900

    boj 아보와 킨텍스

---
+++ boj_silver/problem/num32206.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+    cin>>N;
+    string s;cin>>s;
+    cout<<N*25+26;
+    return 0;
+}
