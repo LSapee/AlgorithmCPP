8c90d67dd5933befee1119ee3d88e6db269673ea
LSapee <dlworhks94@gmail.com>
2024-09-13 23:39:58 +0900

    boj 알고리즘 수업 - 알고리즘의 수행 시간 5

---
+++ boj_bronze/problem/num24266.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    long long n;cin>>n;
+    cout<<n*n*n<<"\n";
+    cout<<3;
+
+    return 0;
+}
