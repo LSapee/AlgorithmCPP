91bea2e046a89ff187c2378eb085c75d901fbd53
LSapee <dlworhks94@gmail.com>
2024-10-24 22:55:12 +0900

    leetcode Transpose Matrix

---
+++ leetCode/num867.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int n =matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> arr(m,vector<int>(n,-1));
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                arr[j][i] = matrix[i][j];
+            }
+        }
+        return arr;
+    }
+};
