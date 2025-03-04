3675e8ba416115e8137c0171054ed98dfdf1c182
LSapee <dlworhks94@gmail.com>
2025-03-04 21:40:29 +0900

    leetcode Partition Array According to Given Pivot (deque를 사용하여 시간 단축)

--- leetCode/num2161.cpp
+++ leetCode/num2161.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        queue<int> Q;
-        queue<int> Q2;
-        int cnt =0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==pivot){
-                cnt++;
-                continue;
-            }else if(nums[i]<pivot){
-                Q.push(nums[i]);
-            }else{
-                Q2.push(nums[i]);
-            }
+        int st =0;
+        int n = nums.size();
+        int ed =  n-1;
+        vector<int> ans(n,0);
+        deque<int> dq;
+        for(int i=0; i<n; i++){
+            if(nums[i]==pivot) dq.push_front(pivot);
+            else if(nums[i]<pivot)ans[st++]= nums[i];
+            else dq.push_back(nums[i]);
         }
-        vector<int> ans;
-        while(!Q.empty()){
-            ans.push_back(Q.front());Q.pop();
-        }
-        while(cnt--)ans.push_back(pivot);
-        while(!Q2.empty()){
-            ans.push_back(Q2.front());Q2.pop();
+        while(!dq.empty()){
+            ans[st++]=dq.front();dq.pop_front();
         }
         return ans;
     }
