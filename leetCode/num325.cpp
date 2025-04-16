b1b3dd1a13d34cc06df0321e828d8b535435bc3b
LSapee <dlworhks94@gmail.com>
2025-04-16 22:05:56 +0900

    leetcode Maximum Size Subarray Sum Equals k

--- leetCode/num325.cpp
+++ leetCode/num325.cpp
@@ -3,17 +3,17 @@ public:
 #define LL long long
     int maxSubArrayLen(vector<int>& nums, int k) {
         int n = nums.size();
-        LL prifixSum= 0;
+        LL prefixSum= 0;
         LL sub =0;
-        map<LL,LL> mp; // unordered_map<LL,LL> mp << 사용시 시간 단축이 더 됨..........
+        unordered_map<LL,LL> mp; // unordered_map<LL,LL> mp << 사용시 시간 단축이 더 됨..........
         for(int i=0; i<n; i++){
-            prifixSum += nums[i];
-            if(prifixSum==k) sub = i+1;
-            if(mp.find((LL)prifixSum-k)!= mp.end()){
-                sub = max(sub,(LL)i-mp[prifixSum-k]);
+            prefixSum += nums[i];
+            if(prefixSum==k) sub = i+1;
+            if(mp.find((LL)prefixSum-k)!= mp.end()){
+                sub = max(sub,(LL)i-mp[prefixSum-k]);
             }
-            if(mp.find(prifixSum)==mp.end()){
-                mp[prifixSum]=i;
+            if(mp.find(prefixSum)==mp.end()){
+                mp[prefixSum]=i;
             }
         }
 
