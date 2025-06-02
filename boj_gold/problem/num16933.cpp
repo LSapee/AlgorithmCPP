8c7f19a7ee104442548b70133d79d7243183b700
LSapee <dlworhks94@gmail.com>
2025-06-02 17:10:56 +0900

    boj 벽 부수고 이동하기 3 (수정) 기존 코드의 경우 통과하면 안되는데 통과하여 해당 문제에 대해 데이터 추가 요청하고 코드 수정

--- boj_gold/problem/num16933.cpp
+++ boj_gold/problem/num16933.cpp
@@ -13,7 +13,7 @@ struct T{
     int x;
     int y;
     int z;
-    int d; // 낮인지 밤인지 구분을 위한 false는 낮 true는 밤
+    int d; //낮은 0 밤은 1
 };
 void input(){
     cin>>N>>M>>K;
@@ -22,24 +22,24 @@ void input(){
         inputMap.push_back(s);
     }
 }
-void moveOnly(queue<T> &Q, T cur,bool &isStop){ // 벽을 부수지 못하는 이동
+void moveOnly(queue<T> &Q, T cur){ // 벽을 부수지 못하는 이동
     for(int i=0; i<4; i++){
         int x = dx[i]+cur.x;
         int y = dy[i]+cur.y;
-        int d = cur.d== 0?1:0;
+        int d = cur.d == 0 ?1:0;
         if(x<0||x>=N||y<0||y>=M)continue;
         if(arr[d][cur.z][x][y] !=0 || inputMap[x][y] =='1')continue;
         arr[d][cur.z][x][y] = arr[cur.d][cur.z][cur.x][cur.y] +1;
         Q.push({x,y,cur.z,d});
-        isStop= false;
     }
 }
 void breakAndMove(queue<T> &Q, T cur){ // 벽을 부수면서 이동.
+    if(cur.d == 1)return ;
     for(int i=0; i<4; i++){
         int x = dx[i]+cur.x;
         int y = dy[i]+cur.y;
-        int z = cur.z+1;
-        int d = cur.d == 0?1:0;
+        int z = cur.z + 1;
+        int d = 1; // 벽을 부수면 무조껀 밤으로
         if(x<0||x>=N||y<0||y>=M||z>K)continue;
         if(arr[d][z][x][y]!=0)continue;
         arr[d][z][x][y] = arr[cur.d][cur.z][cur.x][cur.y]+1;
@@ -47,25 +47,25 @@ void breakAndMove(queue<T> &Q, T cur){ // 벽을 부수면서 이동.
     }
 }
 void stopAndWaitAfternoon(queue<T> &Q, T cur){
-    int d = cur.d ==0 ?1:0;
+    int d = 0; //밤일때만 기다리니 기다린 후는 무조껀 낮으로.
     int x = cur.x;
     int y = cur.y;
     int z = cur.z;
-    if(x==N-1&&y==M-1)return ; // 이미 도착지점인데 저녁이라 이동 못 했을때 패스
+    if(x==N-1&&y==M-1) return ; // 이미 도착지점인데 저녁이라 이동 못 했을때 패스
     if(arr[d][z][x][y] != 0) return ;
     arr[d][z][x][y] = arr[cur.d][cur.z][cur.x][cur.y]+1;
     Q.push({x,y,z,d});
 }
+
 void bfs(){
     queue<T> Q;
     Q.push({0,0,0,0});
     arr[0][0][0][0] = 1;
     while(!Q.empty()){
         auto cur = Q.front(); Q.pop();
-        bool isStop =true;
-        moveOnly(Q,cur,isStop);
-        if(isStop && cur.d)stopAndWaitAfternoon(Q,cur); //이동을 못했고 현재 저녁이라 벽을 부술 수 없어서 기다려야 하는 상황일때.
-        if(!cur.d)breakAndMove(Q,cur);
+        moveOnly(Q,cur);
+        if(cur.d == 1)stopAndWaitAfternoon(Q,cur); //이동을 못했고 현재 저녁이라 벽을 부술 수 없어서 기다려야 하는 상황일때.
+        if(cur.d == 0)breakAndMove(Q,cur);
     }
 }
 
@@ -80,25 +80,9 @@ int main(){
         if(arr[0][i][N-1][M-1] !=0)ans= min(ans,arr[0][i][N-1][M-1]);
         if(arr[1][i][N-1][M-1] !=0)ans= min(ans,arr[1][i][N-1][M-1]);
     }
+
     if(ans==INT_MAX)cout<<-1;
     else cout<<ans;
     return 0;
 }
 
-
-//
-//for(int i=0; i<=K;i++){
-//cout<<"부숴버린 벽의 갯수 : "<<i<<"\n";
-//for(int j=0; j<N; j++){
-//for(int k =0; k<M; k++){
-//cout<<arr[0][i][j][k]<<" ";
-//}
-//cout<<"\n";
-//}
-//for(int j=0; j<N; j++){
-//for(int k =0; k<M; k++){
-//cout<<arr[1][i][j][k]<<" ";
-//}
-//cout<<"\n";
-//}
-//}
