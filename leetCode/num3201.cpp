a53b73b60e9c8fd6eb0dde6315174f0810b4b5c1
LSapee <dlworhks94@gmail.com>
2025-07-16 16:20:19 +0900

    leetcode Find the Maximum Length of Valid Subsequence I

--- leetCode/num3201.cpp
+++ leetCode/num3201.cpp
@@ -1,3 +1,18 @@
-//
-// Created on 2025. 7. 16..
-//
+class Solution {
+public:
+    int dx[4] = {0,0,1,1};
+    int dy[4] = {0,1,0,1};
+    int maximumLength(vector<int>& nums) {
+        int ans =0;
+        for(int i=0; i<4; i++){
+            int cnt = 0;
+            for(int a : nums){
+                int k =cnt%2;
+                if(k == 0 && a%2 == dx[i])cnt++;
+                else if(k==1 && a%2 == dy[i])cnt++;
+            }
+            ans = max(ans,cnt);
+        }
+        return ans;
+    }
+};
