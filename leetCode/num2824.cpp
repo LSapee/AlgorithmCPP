7ac271940b7754b10ab05f9e52344ec6a8e2cf21
LSapee <dlworhks94@gmail.com>
2025-07-29 19:55:24 +0900

    leetcode Count Pairs Whose Sum is Less than Target

---
+++ leetCode/num2824.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int n = nums.size();
+        int ans =0;
+        for(int i=0; i<n; i++){
+            for(int j =i+1; j<n; j++){
+                if(nums[i]+nums[j]<target)ans++;
+            }
+        }
+        return ans;
+    }
+};
