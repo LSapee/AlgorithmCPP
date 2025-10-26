53defb07854c87dc135fc6acde68b168f0a4cbd4
LSapee <dlworhks94@gmail.com>
2025-10-26 20:19:55 +0900

    leetcode Find the Distance Value Between Two Arrays

---
+++ leetCode/num1385.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        int n = arr1.size();
+        int m =arr2.size();
+        int ans = n;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(abs(arr1[i]-arr2[j]) <= d){
+                    ans--;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
