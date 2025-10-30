be42e09741a89c8c101fd8f90eb433954283931c
LSapee <dlworhks94@gmail.com>
2025-10-30 21:28:13 +0900

    leetcode Percentage of Letter in String

---
+++ leetCode/num2278.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int n = s.size();
+        int cnt =0;
+        for(int i=0; i<n; i++) if(s[i]==letter)cnt++;
+        return (cnt*100)/n;
+    }
+};
