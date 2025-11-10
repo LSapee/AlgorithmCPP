94cb0e4138039080498ba01b9c71df87e3b801d9
LSapee <dlworhks94@gmail.com>
2025-11-10 17:21:18 +0900

    leetcode Minimum Number of Chairs in a Waiting Room

---
+++ leetCode/num3168.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumChairs(string s) {
+        int ans =0;
+        int cnt =0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='E')cnt++;
+            else cnt--;
+            ans= max(ans,cnt);
+        }
+        return ans;
+    }
+};
