943704fbf46f6e8b332e43ade2f691f0811e89c2
LSapee <dlworhks94@gmail.com>
2025-11-21 22:50:24 +0900

    leetcode Number of Arithmetic Triplets

---
+++ leetCode/num2367.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        int n= nums.size();
+        int ans =0;
+        for(int i=0; i<n-2; i++){
+            for(int j=i+1; j<n-1; j++){
+                if(nums[j]-nums[i]!=diff)continue;
+                for(int k=j+1; k<n; k++){
+                    if(nums[k]-nums[j] == diff)ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
