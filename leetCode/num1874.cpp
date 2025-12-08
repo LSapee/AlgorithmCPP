bec64aff43ada93fc05048b3b9bbc06379958547
LSapee <dlworhks94@gmail.com>
2025-12-08 13:40:06 +0900

    leetcode Minimize Product Sum of Two Arrays

---
+++ leetCode/num1874.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minProductSum(vector<int>& nums1, vector<int>& nums2) {
+        int ans = 0;
+        int n = nums1.size();
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        for(int i=0; i<n; i++){
+            ans += nums1[i]*nums2[n-1-i];
+        }
+        return ans;
+    }
+};
