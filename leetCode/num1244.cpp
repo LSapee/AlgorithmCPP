d436869d2ca382e5b178fb3fed080e2ea454794f
LSapee <dlworhks94@gmail.com>
2026-01-05 14:44:53 +0900

    leetcode Design A Leaderboard

--- leetCode/num1244.cpp
+++ leetCode/num1244.cpp
@@ -2,7 +2,8 @@ class Leaderboard {
 public:
     // 그냥 map일때 12ms
     // unordered_map 일때 8ms
-    //  map -> erase  에서 map[key] = 0으로 처리하면 11ms으로 느려짐. 
+    //  map -> erase  에서 map[key] = 0으로 처리하면 11ms으로 느려짐.
+    // 4ms ~6ms로 나옴 O(N logN)
     unordered_map<int,int> mp;
     vector<int> arr;
     Leaderboard() {
@@ -16,7 +17,6 @@ public:
 
     int top(int K) {
         priority_queue<int> pq;
-        vector<int> temp;
         for(auto a : mp)pq.push(a.second);
         int ans = 0;
         while(K>0&& !pq.empty()){
