ac98b1e161eacf80e92ca8ecb40c19b04071ee1e
LSapee <dlworhks94@gmail.com>
2026-01-05 14:41:44 +0900

    leetcode Design A Leaderboard

---
+++ leetCode/num1244.cpp
@@ -0,0 +1,44 @@
+class Leaderboard {
+public:
+    // 그냥 map일때 12ms
+    // unordered_map 일때 8ms
+    //  map -> erase  에서 map[key] = 0으로 처리하면 11ms으로 느려짐. 
+    // map.find 제거시 4ms
+
+    unordered_map<int,int> mp;
+    int arr[10003];
+    Leaderboard() {
+
+    }
+
+    void addScore(int playerId, int score) {
+        arr[playerId] += score;
+        mp[playerId]+= score;
+    }
+
+    int top(int K) {
+        priority_queue<int> pq;
+        vector<int> temp;
+        for(auto a : mp)pq.push(a.second);
+        int ans = 0;
+        while(K>0&& !pq.empty()){
+            ans+=pq.top();
+            pq.pop();
+            K--;
+        }
+        return ans;
+    }
+
+    void reset(int playerId) {
+        arr[playerId] = 0;
+        mp.erase(playerId);
+    }
+};
+
+/**
+ * Your Leaderboard object will be instantiated and called as such:
+ * Leaderboard* obj = new Leaderboard();
+ * obj->addScore(playerId,score);
+ * int param_2 = obj->top(K);
+ * obj->reset(playerId);
+ */
