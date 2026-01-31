399c2e3f81ddeb8bb757b1b7f8f46215fdee168b
LSapee <dlworhks94@gmail.com>
2026-01-31 22:40:46 +0900

    atcoder 20260131 A - Append s(solved)

---
+++ atcoder/20260131/num01.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string S;
+void input(){
+    cin>>S;
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    input();
+    cout<<S<<"s";
+    return 0;
+}
