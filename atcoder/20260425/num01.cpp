8d6e29c70ebbee467a1caee332a233413fd85356
LSapee <dlworhks94@gmail.com>
2026-04-30 18:59:36 +0900

    atcoder 20260425 A - 455 (solved)

---
+++ atcoder/20260425/num01.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int A,B,C;
+    cin>>A>>B>>C;
+    if (A!=B && B==C) cout<<"Yes";
+    else cout<<"No";
+    return 0;
+}
